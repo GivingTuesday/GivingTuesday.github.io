e56b577c16487c5ac04fdb3e36634766c980bf89
Ashleigh Bair <ashleigh1994626@yahoo.co.uk>
2015-11-30 11:35:14 +0000

    twitter update

--- js/twitter.js
+++ js/twitter.js
@@ -18,6 +18,70 @@ alert("Twitter login failed, Please try again");
 //todo when the OAuth flow failed
 })
 };
- 
-//Twitter buttons fuction
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s),p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+maxL=115;
+var bName = navigator.appName;
+function taLimit(taObj) {
+	if (taObj.value.length==maxL) return false;
+	return true;
+}
+
+function taCount(taObj,Cnt) { 
+	objCnt=createObject(Cnt);
+	objVal=taObj.value;
+	if (objVal.length>maxL) objVal=objVal.substring(0,maxL);
+	if (objCnt) {
+		if(bName == "Netscape"){	
+			objCnt.textContent=maxL-objVal.length;}
+		else{objCnt.innerText=maxL-objVal.length;}
+	}
+	return true;
+}
+function createObject(objId) {
+	if (document.getElementById) return document.getElementById(objId);
+	else if (document.layers) return eval("document." + objId);
+	else if (document.all) return eval("document.all." + objId);
+	else return eval("document." + objId);
+}
+
+function tweet(){
+//    authMe();
+    alert("I'm here")
+//    $(document).ready(function(){
+//        alert("Checking...")
+//        $("#yeslink").click(function(){
+//            alert("logic check")
+//            if($(this).prop("checked") == true){
+//                var link = " j.mp/AlertGT";
+//                var tweettext = $("#tweettext");
+//                tweettext = tweettext+link;
+//                alert("tweettext"+tweettext)
+////                twttr.widgets.createShareButton('https://dev.twitter.com/', tweettext,
+////                                                {
+////                    text: tweettext
+////                }
+////                                               );
+//            }
+//            else if($(this).prop("checked") == false){
+//                var tweetstring =  $("#tweettext");
+//                alert("tweetstring"+tweetstring)
+////                twttr.widgets.createShareButton('https://dev.twitter.com/', tweetstring,
+////                                                {
+////                    text: tweetstring
+////                }
+////                                               );
+//            }
+//        });
+//    });
+    
+    var linkyes = document.getElementById("yeslink").value;
+    
+    if (linkyes == "on") {
+        var link = " j.mp/AlertGT";
+        var tweettext = document.getElementById("tweettext").value;
+        console.log(tweettext);
+        tweettext = tweettext + link;
+        console.log(tweettext);
+    }
+    
+}
